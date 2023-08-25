52a41beac87f8698c7e338f97f406bacaa5e3667
desokan <thefdmm@gmail.com>
2023-08-25 12:34:57 +0100

    added about us component to the homepage

---
+++ src/components/landingpage/AboutUs.js
@@ -0,0 +1,72 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link } from "react-router-dom";
+
+const AboutUs = () => {
+  return (
+    <div className="overflow-hidden bg-white pb-20 lg:pt-20 mx-auto max-w-7xl">
+      <div className="mx-auto max-w-7xl lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
+          <div className="lg:col-end-1 lg:w-full lg:max-w-lg">
+            <h2 className="text-3xl font-bold tracking-tight text-indigo-800 sm:text-3xl">
+              Elevating Voices, Inspiring Minds
+            </h2>
+            <p className="mt-6 text-xl leading-8 text-gray-600">
+              We are the leading platform empowering speakers to leave an
+              indelible mark on the world's stage. Our mission is to connect
+              distinguished speakers with top-tier event organisers.
+            </p>
+            <p className="mt-6 text-sm leading-7 text-gray-600">
+              We believe in the power of captivating narratives and
+              transformative ideas. By joining our esteemed community, you gain
+              unparalleled access to global opportunities and audiences eager to
+              be inspired. Your unique voice and expertise have the potential to
+              shape minds, influence hearts, and spark positive change.
+            </p>
+            <div className="mt-10 flex">
+              <Link
+                to="/contact-us"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Become a Speaker <span aria-hidden="true">&rarr;</span>
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
+            <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+              <img
+                src="https://images.unsplash.com/photo-1670272502246-768d249768ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80"
+                alt=""
+                className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+              />
+            </div>
+            <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
+              <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
+                <img
+                  src="https://images.unsplash.com/photo-1605656816944-971cd5c1407f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
+                  alt=""
+                  className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                />
+              </div>
+              <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
+                <img
+                  src="https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&h=842&q=80"
+                  alt=""
+                  className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                />
+              </div>
+              <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
+                <img
+                  src="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
+                  alt=""
+                  className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/components/pagelayout/HomePageLayout.js
+++ src/components/pagelayout/HomePageLayout.js
@@ -1,11 +1,13 @@
 import TopMessage from "../landingpage/TopMessage";
 import IntroCard from "../landingpage/IntroCard";
+import OurPeople from "../landingpage/AboutUs";
 
 const PageCentreLayout = () => {
   return (
     <div className="">
       <TopMessage></TopMessage>
       <IntroCard></IntroCard>
+      <OurPeople></OurPeople>
     </div>
   );
 };
