c4391bb34aed1a09eafdc1c5e7040a1bba40689a
desokan <thefdmm@gmail.com>
2023-08-28 16:44:49 +0100

    Update the TopicsPage component to send a GET request to the server

--- src/pages/TopicsPage.js
+++ src/pages/TopicsPage.js
@@ -1,115 +1,54 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const topics = [
-  {
-    name: "Leadership Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "Security, Risk & Mnagement Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "1Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "2Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "3Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "4Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "5Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "6Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "7Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "8Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "9Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "0Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "11Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-  {
-    name: "12Mental Health & Wellbeing Speakers",
-    description:
-      "We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
-];
-
 const TopicsPage = () => {
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    const fetchTopics = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/topics", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        if (response.status === 200) {
+          const receivedData = await response.json();
+          const topicsExtract = receivedData.data.topics;
+          setTopics(topicsExtract);
+        }
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    fetchTopics();
+  }, []);
+
+  const topicsData = topics.map((topic) => (
+    <li key={topic._id}>
+      <Link to={"#"}>
+        <img
+          className="mx-auto rounded-md h-36 w-full object-cover"
+          src={topic.imageUrl}
+          alt=""
+        />
+        <h3 className="mt-2 mb-2 text-base font-semibold leading-5 tracking-tight text-gray-900">
+          {topic.name}
+        </h3>
+      </Link>
+    </li>
+  ));
+
+  const noTopicsToDisplay = (
+    <h2 className="font-bold text-indigo-800">No topics to display.</h2>
+  );
+
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8 rounded-md mt-8 -mb-20">
@@ -122,20 +61,7 @@ const TopicsPage = () => {
           role="list"
           className="mx-auto my-10 grid max-w-2xl grid-cols-2 gap-x-4 gap-y-12 sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-4"
         >
-          {topics.map((topic) => (
-            <li key={topic.name}>
-              <Link to={"#"}>
-                <img
-                  className="mx-auto rounded-md h-36 w-full object-cover"
-                  src={topic.imageUrl}
-                  alt=""
-                />
-                <h3 className="mt-2 mb-2 text-base font-semibold leading-5 tracking-tight text-gray-900">
-                  {topic.name}
-                </h3>
-              </Link>
-            </li>
-          ))}
+          {topics.length >= 1 ? topicsData : noTopicsToDisplay}
         </ul>
       </div>
     </div>
